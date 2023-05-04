1d717c6a2d6a6a62495fb8f2dab9494317af926f
Irirna <burdina98@mail.ru>
2023-05-04 10:37:32 +0500

    init ticket

--- src/test/java/ManagerTest.java
+++ src/test/java/ManagerTest.java
@@ -21,5 +21,68 @@ public class ManagerTest {
         Ticket[] actual = manager.findAll("DME", "EGO");
         Assertions.assertArrayEquals(expected, actual);
     }
+
+    @Test
+    public void thereAreSeveralTickets() {
+        Repository repo = new Repository();
+        Manager manager = new Manager(repo);
+
+        Ticket ticket1 = new Ticket(1, 600, "DME", "EGO", 100);
+        Ticket ticket2 = new Ticket(2, 400, "DME", "EGO", 110);
+        Ticket ticket3 = new Ticket(3, 200, "DME", " FRU", 150);
+        Ticket ticket4 = new Ticket(4, 300, "FRU", "DME", 130);
+        manager.add(ticket1);
+        manager.add(ticket2);
+        manager.add(ticket3);
+        manager.add(ticket4);
+
+
+        Ticket[] expected = {ticket2, ticket1};
+        Ticket[] actual = manager.findAll("DME", "EGO");
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void thereIsExactlyOneTicket() {
+
+        Repository repo = new Repository();
+        Manager manager = new Manager(repo);
+
+        Ticket ticket1 = new Ticket(1, 600, "DME", "EGO", 100);
+        Ticket ticket2 = new Ticket(2, 400, "DME", "EGO", 110);
+        Ticket ticket3 = new Ticket(3, 200, "DME", " FRU", 150);
+        Ticket ticket4 = new Ticket(4, 300, "FRU", "DME", 130);
+        manager.add(ticket1);
+        manager.add(ticket2);
+        manager.add(ticket3);
+        manager.add(ticket4);
+
+
+        Ticket[] expected = {ticket4};
+        Ticket[] actual = manager.findAll("FRU", "DME");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void noneOfTheTicketsFit() {
+        Repository repo = new Repository();
+        Manager manager = new Manager(repo);
+
+        Ticket ticket1 = new Ticket(1, 600, "DME", "EGO", 100);
+        Ticket ticket2 = new Ticket(2, 400, "DME", "EGO", 110);
+        Ticket ticket3 = new Ticket(3, 200, "DME", " FRU", 150);
+        Ticket ticket4 = new Ticket(4, 300, "FRU", "DME", 130);
+        manager.add(ticket1);
+        manager.add(ticket2);
+        manager.add(ticket3);
+        manager.add(ticket4);
+
+
+        Ticket[] expected = {};
+        Ticket[] actual = manager.findAll("0", "0");
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
 }
 
